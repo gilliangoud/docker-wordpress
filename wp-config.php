52d480fc8e7cb5dbf0d682ecfa11a4a91ae29d73
Gillian Goud <gilliangoud@hotmail.com>
2022-11-01 15:43:37 +0100

    Verified working

--- wp-config.php
+++ wp-config.php
@@ -2,6 +2,17 @@
 
 define('WP_CONTENT_DIR', '/var/www/wp-content');
 
+# Detect that we're behind a reverse proxy
+if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' ) {
+    $_SERVER['HTTPS'] = 'on';
+}
+
+# Ability to manually set the wordpress URLS
+if ( getenv( 'WP_HOME' ) == 'ANY' || getenv( 'WP_SITEURL' ) == 'ANY' ) {
+    define( 'WP_HOME', ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http' ) . "://$_SERVER[HTTP_HOST]" );
+    define( 'WP_SITEURL', ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http' ) . "://$_SERVER[HTTP_HOST]" );
+}
+
 $table_prefix  = getenv('TABLE_PREFIX') ?: 'wp_';
 
 foreach ($_ENV as $key => $value) {
