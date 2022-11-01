b76f7d66738b7e1bc6d7af932c16b112173577bd
Gillian Goud <gilliangoud@hotmail.com>
2022-11-01 14:36:53 +0100

    hopefully fixed redirect loop

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,22 +9,18 @@ if [ ! "$(ls -A "/var/www/wp-content" 2>/dev/null)" ]; then
     # Copy wp-content from Wordpress src to volume
     cp -r /usr/src/wordpress/wp-content /var/www/
     chown -R nobody.nobody /var/www
-
-    # Generate secrets
-    curl -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php
 fi
 
-# Check if volume is empty
+# Generate secrets
 if [ ! -f /usr/src/wordpress/wp-secrets.php ]; then
     cat <<<"Setting up wp-secrets."
     echo "<?php" > /usr/src/wordpress/wp-secrets.php
     chown nobody.nobody /usr/src/wordpress/wp-secrets.php
     chmod 640 /usr/src/wordpress/wp-secrets.php
-    # Generate secrets
     curl -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php
 fi
 
-su -s /bin/bash nobody -c 'wp plugin install nginx-cache/ --activate --path="/usr/src/wordpress"'
+# su -s /bin/bash nobody -c 'wp plugin install nginx-cache --activate --path="/usr/src/wordpress"'
 
 # rm -rf /usr/src/wordpress/wp-content
 # ln -s /var/www/wp-content /usr/src/wordpress
